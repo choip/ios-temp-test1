41fc88ac7ff8751634c4349405051af3291ae0e2
PD Choi <PD@macintosh.local>
2017-04-07 21:10:58 +0530

    just added comments

--- ios-temp-test1/ViewController.swift
+++ ios-temp-test1/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Additional Comments
     }
 
     override var representedObject: Any? {
