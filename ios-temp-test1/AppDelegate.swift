ee2be078cd0c0b49b1969ca2a1658d7a345c8535
PD Choi <PD@macintosh.local>
2017-04-07 20:59:43 +0530

    Initial Commit

---
+++ ios-temp-test1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ios-temp-test1
+//
+//  Created by PD Choi on 07/04/17.
+//  Copyright © 2017 PD Choi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ios-temp-test1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-temp-test1
+//
+//  Created by PD Choi on 07/04/17.
+//  Copyright © 2017 PD Choi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
